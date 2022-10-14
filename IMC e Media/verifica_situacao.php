bd6c000e0315b86a22ffdf3f8da125a731101c20
101kbps <carol_n_nv@hotmail.com>
2022-10-14 10:04:44 -0300

    commit to ltpi-3ads

--- IMC e Media/verifica_situacao.php
+++ IMC e Media/verifica_situacao.php
@@ -4,11 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Parte1</title>
+    <title>VERIFICAR SITUAÇÃO</title>
 </head>
 <body>
-
-//usar filter_input//
     
 </body>
 </html>
